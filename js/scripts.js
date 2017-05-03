4e6f9d1047ef0cee4425ad7fef1e516cd5ed214c
Brendan Hanna and Daniel Lopez <daniel@>
2017-05-03 16:23:13 -0700

    working up to two or more letter words

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ var vowels = ["a", "e", "i", "o", "u"]
 
 
 
-
 // front-end below
 
 $(function() {
@@ -25,7 +24,15 @@ $(function() {
       var userInput = userInput + "way";
     }
     $("#pig-sentence").text(userInput);
-    console.log(userInput.length);
+
+    //words starting with one consonant before a vowel move the consonant to the end and add "ay"
+
+    // make a loop that (checks if the first letter is a consonant)
+    // {move that consonant to the end}
+
+for (var index = 0; value of current index = one of the vowells; index += 1){
+  var x = slice out all index values so far and store them in a varable
+}
 
 
   });
